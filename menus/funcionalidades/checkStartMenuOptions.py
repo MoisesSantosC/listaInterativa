6e297ab9b2ce45581024587b174209b85c47f6ad
Moises Santos Carvalho <moisessantos140515@gmail.com>
2021-09-15 19:32:49 -0300

    Módulo para verificar o input do usuário

---
+++ menus/funcionalidades/checkStartMenuOptions.py
@@ -0,0 +1,26 @@
+# Importando Módulos
+from menus.funcionalidades.cleanMenu import clear
+from menus.programExitMenu import programExit
+import menus.startMenu as home
+
+
+# Verifica as opções digitadas do menu principal
+def checkMenuOptions(op):
+    op = op.strip()  # elimina os espaços no começo e final da string
+    if op == '1':
+        clear()  # limpa a tela do terminal
+        print('Você selecionou a opção de Criar uma Lista\n')
+        
+    elif op == '2':
+        clear()  # limpa a tela do terminal
+        print('Você selecionou a opção de Ver Listas\n')
+        
+    elif op == '3':
+        clear()  # limpa a tela do terminal
+        print('Você selecionou a opção de Sair\n')
+        programExit()  # Menu da terceira opção
+        
+    else:
+        clear()
+        print('Selecione uma das opções (1, 2 ou 3)\n')
+        home.menu()  # Executando o menu
