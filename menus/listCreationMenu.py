e3c4d354dc6431a106bd488a22ed746995f7ab6f
Moises Santos Carvalho <moisessantos140515@gmail.com>
2021-09-15 15:18:14 -0300

    Criado a função que verifica o input do usuário

--- menus/listCreationMenu.py
+++ menus/listCreationMenu.py
@@ -1,3 +1,7 @@
+# Importando Módulo
+from menus.cleanMenu import clear
+import menus.startMenu as inicio
+
 # Menu de Criação de listas
 def listCreation():
     msg_menu = f'{5*"="} Menu de Criação de Listas {5*"="}'
@@ -9,3 +13,25 @@ def listCreation():
     
     # Capturando o valor de entrada do usuário
     create_op = input('Escolha sua opção (1, 2 ou 3): ')
+    checkOptions(create_op)
+
+# Verifica as opções digitadas do menu principal
+def checkOptions(op):
+    op = op.strip()  # elimina os espaços no começo e final da string
+    if op == '1':
+        clear()  # limpa a tela do terminal
+        print('Você selecionou a opção de Criar somente o nome da lista\n')
+        
+    elif op == '2':
+        clear()  # limpa a tela do terminal
+        print('Você selecionou a opção de Criar nome e adicionar item\n')
+        
+    elif op == '3':
+        clear()  # limpa a tela do terminal
+        print('Você selecionou a opção de Voltar ao menu inicial\n')
+        inicio.menu()  # Menu da terceira opção
+        
+    else:
+        clear()
+        print('Selecione uma das opções (1, 2 ou 3)\n')
+        listCreation()  # Executando o menu para criar lista
