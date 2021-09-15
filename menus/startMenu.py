6463d62d4e6c60ffe23c4f9c6deb3d79622460b6
Moises Santos Carvalho <moisessantos140515@gmail.com>
2021-09-15 19:31:23 -0300

    Deixando o arquivo responsável somente do menu

--- menus/startMenu.py
+++ menus/startMenu.py
@@ -1,6 +1,6 @@
 # Importando os Módulos
-from menus.programExitMenu import programExit
 from menus.funcionalidades.cleanMenu import clear
+from menus.funcionalidades.checkStartMenuOptions import checkMenuOptions
 
 # Menu inicial
 def menu():
@@ -16,24 +16,3 @@ def menu():
     # Capturando o valor de entrada do usuário
     menu_op = input('Escolha sua opção (1, 2 ou 3): ')
     checkMenuOptions(menu_op)
-
-# Verifica as opções digitadas do menu principal
-def checkMenuOptions(op):
-    op = op.strip()  # elimina os espaços no começo e final da string
-    if op == '1':
-        clear()  # limpa a tela do terminal
-        print('Você selecionou a opção de Criar uma Lista\n')
-        
-    elif op == '2':
-        clear()  # limpa a tela do terminal
-        print('Você selecionou a opção de Ver Listas\n')
-        
-    elif op == '3':
-        clear()  # limpa a tela do terminal
-        print('Você selecionou a opção de Sair\n')
-        programExit()  # Menu da terceira opção
-        
-    else:
-        clear()
-        print('Selecione uma das opções (1, 2 ou 3)\n')
-        menu()  # Executando o menu
