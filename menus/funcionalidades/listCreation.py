b83939e4cd7aeac232ca71ddd1aac7f645713b14
Moises Santos Carvalho <moisessantos140515@gmail.com>
2021-09-28 19:00:11 -0300

    Adicionando vários itens em um input só

--- menus/funcionalidades/listCreation.py
+++ menus/funcionalidades/listCreation.py
@@ -7,14 +7,23 @@ items_list = []
 # Função para criar a lista
 def create():
     key = input('Digite o nome da lista: ').strip().capitalize()
+    
+    print('\nDigite todos os itens da lista separados por vírgula')
+    print('Exemplo: Item 1, Item 2, Item 3\n')
+    
+    # Deixando cada item digitado com a primeira letra maiúscula
+    items = input('Digite os itens: ').title()
+    
+    # Adicionando na lista todos os itens separados por vírgula
+    items_list = items.split(',')
+    
     while True:
-        item = input('Digite o nome do item: ')
         add_more_item = input('Deseja adicionar mais item? [s]im ou [n]ão: ').strip().lower()
         
         if add_more_item == 's':
+            item = input('Digite o nome do item: ').strip()
             items_list.append(item)
         else:
-            items_list.append(item)
             list_dict[key] = items_list
             
             startMenu.menu()
