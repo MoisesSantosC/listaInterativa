2b864e818143cad141edc88e8aa3128998eb78bc
Moises Santos Carvalho <moisessantos140515@gmail.com>
2021-10-13 09:01:44 -0300

    Update - Importando o módulo back to menu e fazendo verificação de lista vazia

--- menus/funcionalidades/listView.py
+++ menus/funcionalidades/listView.py
@@ -1,22 +1,29 @@
 from menus.funcionalidades.listCreation import list_dict
+from menus.funcionalidades import backToMenu
+
 
 def view():
-    print('Selecione a lista pelo nome\n')
-    
-    print('SUAS LISTAS')
-    print(' N° | NOME DA LISTA')
-    
-    # Contador de listas criadas
-    count = 0
-    
-    # Mostrando os nomes das listas
-    for key in list_dict:
-        count += 1
-        print(f'[{count}] {key.title()}')
-    
-    # Entrada do usuário para selecionar a lista
-    select = input('\nNome da lista: ')
+    if not list_dict == {}:
+        print('Selecione a lista pelo nome\n')
 
-    # Visualizando os itens da lista selecionada
-    if select in list_dict:
-        print(list_dict[select])
+        print('SUAS LISTAS')
+        print(' N° | NOME DA LISTA')
+
+        # Contador de listas criadas
+        count = 0
+
+        # Mostrando os nomes das listas
+        for key in list_dict:
+            count += 1
+            print(f'[{count}] {key.title()}')
+
+        # Entrada do usuário para selecionar a lista
+        select = input('\nNome da lista: ')
+
+        # Visualizando os itens da lista selecionada
+        if select in list_dict:
+            print(list_dict[select])
+
+    else:
+        print('Nenhuma lista foi criada ainda!\n')
+        backToMenu.back()
