5786b9dbfb1070893c6a66bc7ab377c13033ea83
Moises Santos Carvalho <moisessantos140515@gmail.com>
2021-10-20 10:32:53 -0300

    Removido mensagens da opção selecionada

--- menus/funcionalidades/checkStartMenuOptions.py
+++ menus/funcionalidades/checkStartMenuOptions.py
@@ -5,23 +5,21 @@ import menus.startMenu as home
 from menus.funcionalidades import listCreation
 from menus.funcionalidades import listView
 
+
 # Verifica as opções digitadas do menu principal
 def checkMenuOptions(op):
     if op == '1':
         clear()  # limpa a tela do terminal
-        print('Você selecionou a opção de Criar uma Lista\n')
-        listCreation.create()        
-        
+        listCreation.create()
+
     elif op == '2':
         clear()  # limpa a tela do terminal
-        print('Você selecionou a opção de Ver Listas\n')
         listView.view()
-        
+
     elif op == '3':
         clear()  # limpa a tela do terminal
-        print('Você selecionou a opção de Sair\n')
         programExit()
-        
+
     else:
         print('Selecione uma das opções (1, 2 ou 3)\n')
         home.menu()  # Executando o menu
