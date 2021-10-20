6e81c47aa0ef1bfb0f18cc00ca057f7df3fd3981
Moises Santos Carvalho <moisessantos140515@gmail.com>
2021-10-20 11:37:25 -0300

    Adicionado a verificação de entrada

--- menus/programExitMenu.py
+++ menus/programExitMenu.py
@@ -1,11 +1,26 @@
+from menus.funcionalidades import backToMenu
+from menus.funcionalidades import cleanMenu
+
+
 # Menu de saída do programa
 def programExit():
-    msg_menu = '== Deseja realmente sair do programa? ==' 
-    
-    print(msg_menu)
-    print('[1] - Sim')
-    print('[2] - Não')
-    print(len(msg_menu) * "=")
-    
-    # Capturando o valor de entrada do usuário
-    quit_op = input('Escolha sua opção (1 ou 2): ')
+    while True:
+        msg_menu = '== Deseja realmente sair do programa? =='
+
+        print(msg_menu)
+        print('[1] - Sim')
+        print('[2] - Não')
+        print(len(msg_menu) * "=")
+
+        # Capturando o valor de entrada do usuário
+        quit_op = input('Escolha sua opção (1 ou 2): ')
+
+        # Verificando as opções digitadas
+        if not (quit_op == '1' or quit_op == '2'):
+            print('Opção não encontrada. Por favor, tente novamente!')
+        elif quit_op == '1':
+            break
+        elif quit_op == '2':
+            cleanMenu.clear()
+            backToMenu.back()
+            break
