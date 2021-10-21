14bc50e07294d4d22ac8688d1d4d7b4cb5421c7f
Moises Santos Carvalho <moisessantos140515@gmail.com>
2021-10-21 06:33:18 -0300

    Modificado o menu de visualização

--- menus/funcionalidades/listView.py
+++ menus/funcionalidades/listView.py
@@ -4,20 +4,19 @@ from menus.funcionalidades import backToMenu
 
 def view():
     if not list_dict == {}:
-        print('Selecione a lista pelo nome\n')
-        print('SUAS LISTAS')
+        print('SUAS LISTAS'.center(29))
+        print(29 * '=')
 
         # Mostrando os nomes das listas
         for indice, valor in enumerate(list_dict):
             print(f'[{indice + 1}] - {valor}')
 
-        # Entrada do usuário para selecionar a lista
-        select = input('\nNome da lista: ')
-
-        # Visualizando os itens da lista selecionada
-        for indice, valor in enumerate(list_dict[select]):
-            print(valor.strip())
+        print(29 * '=')
 
     else:
-        print('Nenhuma lista foi criada ainda!\n')
+        msg = 'Nenhuma lista foi criada ainda!'
+
+        print(f'{len(msg) * "="}')
+        print(msg)
+        print(f'{len(msg) * "="}')
         backToMenu.back()
