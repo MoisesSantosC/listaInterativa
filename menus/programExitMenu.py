15af5334a5f489404a600b727a825bb8ba49773a
Moises Santos Carvalho <moisessantos140515@gmail.com>
2021-10-21 06:46:35 -0300

    Otimizando o menu de saída do programa

--- menus/programExitMenu.py
+++ menus/programExitMenu.py
@@ -7,18 +7,22 @@ def programExit():
     while True:
         msg_menu = '== Deseja realmente sair do programa? =='
 
-        print(msg_menu)
-        print('[1] - Sim')
-        print('[2] - Não')
-        print(len(msg_menu) * "=")
+        print(f'''{msg_menu}
+             \r[1] - Sim
+             \r[2] - Não
+             \r{len(msg_menu) * "="}''')
 
         # Capturando o valor de entrada do usuário
         quit_op = input('Escolha sua opção (1 ou 2): ')
 
         # Verificando as opções digitadas
         if not (quit_op == '1' or quit_op == '2'):
-            print('Opção não encontrada. Por favor, tente novamente!')
+            cleanMenu.clear()
+            print('Opção não encontrada. Por favor, tente novamente!\n')
+            continue
         elif quit_op == '1':
+            cleanMenu.clear()
+            print('Programa Finalizado!')
             break
         elif quit_op == '2':
             cleanMenu.clear()
