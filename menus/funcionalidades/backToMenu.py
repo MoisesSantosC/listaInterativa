7dc3b07b367d99c0f75159ad7b6093236a031016
Moises Santos Carvalho <moisessantos140515@gmail.com>
2021-10-21 06:35:52 -0300

    Modificado a visualização do menu

--- menus/funcionalidades/backToMenu.py
+++ menus/funcionalidades/backToMenu.py
@@ -5,16 +5,17 @@ from menus.funcionalidades import cleanMenu
 
 def back():
     while True:
-        msg = '''O que deseja fazer agora?
+        print(f'''O que deseja fazer agora?
         \r[1] Menu Principal
         \r[2] Sair do Programa
-        '''
+        \r{31 * "="}''')
 
-        print(msg)
-        op = input('Sua opção: ')
+        op = input('Escolha sua opção (1 ou 2): ')
 
         if not (op == '1' or op == '2'):
-            print('\nVerifique a opção digitada e tente novamente\n')
+            cleanMenu.clear()
+            print('Verifique a opção digitada e tente novamente!\n')
+            print(31 * '=')
             continue
         elif op == '1':
             startMenu.menu()
